2a8b618d2230669564ee357e84e39686f6baaad0
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2024-01-22 14:29:41 -0300

    kayo.cezario

--- curso-loiane-basico/src/main/java/exercicios/aula20/exercicio6/JogoDaVelha.java
+++ curso-loiane-basico/src/main/java/exercicios/aula20/exercicio6/JogoDaVelha.java
@@ -9,15 +9,28 @@ public class JogoDaVelha {
 		Character[][] tabuleiro = new Character[3][3];
 		final char JOGADOR_A = 'X';
 		final char JOGADOR_B = 'O';
-		for (Character[] linha : tabuleiro) {
-			for (Character coluna : tabuleiro) {
-				
-			}
-
-		}
-		System.out.println(" | | ");
-
+		
+		// mostra o tabuleiro no terminal
+		mostraTabuleiro(tabuleiro);
+		
 		sc.close();
+	}
+
+	private static void mostraTabuleiro(Character[][] tabuleiro) {
+		System.out.println("-------");
+		for (Character[] linha : tabuleiro) {
+			System.out.print("|");
+			for (Character coluna : linha) {
+				if (coluna != null) {
+					System.out.print(coluna);
+				} else {
+					System.out.print(" ");
+				}
+				System.out.print("|");
+			}
+			System.out.println();
+			System.out.println("-------");
+		}
 	}
 
 }
