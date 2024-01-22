e81dfec40ff7431f09f11abd0a1c716491b2eff7
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2024-01-22 14:19:38 -0300

    Subindo arquivos

---
+++ curso-loiane-basico/src/main/java/exercicios/aula20/exercicio6/JogoDaVelha.java
@@ -0,0 +1,23 @@
+package exercicios.aula20.exercicio6;
+
+import java.util.Scanner;
+
+public class JogoDaVelha {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Character[][] tabuleiro = new Character[3][3];
+		final char JOGADOR_A = 'X';
+		final char JOGADOR_B = 'O';
+		for (Character[] linha : tabuleiro) {
+			for (Character coluna : tabuleiro) {
+				
+			}
+
+		}
+		System.out.println(" | | ");
+
+		sc.close();
+	}
+
+}
