0a3b317c640992178e75f9629325476f1768cccc
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2024-01-23 09:02:10 -0300

    início do projeto crud

---
+++ crud/src/test/java/com/agrotis/trainees/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis.trainees.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
