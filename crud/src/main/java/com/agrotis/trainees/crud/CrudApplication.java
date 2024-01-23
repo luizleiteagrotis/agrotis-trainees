69d633316f2b3cc7d4f12985ddd272175f5471f3
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2024-01-23 15:45:54 -0300

    ajusta pom.xml

--- crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
+++ crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
@@ -8,7 +8,7 @@ public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
-		//
+        System.out.println("Iniciado com sucesso!");
 	}
 
 }
