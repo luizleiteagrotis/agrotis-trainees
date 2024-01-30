cf57a5c0b1876dca1af33ec2e2c1b93058626ebe
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2024-01-30 17:05:25 -0300

    wip(NotaFiscalTipo): inclui classe de exemplo de teste unitário

---
+++ crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalTipoServiceTest.java
@@ -0,0 +1,14 @@
+package com.agrotis.trainees.crud.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NotaFiscalTipoServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
