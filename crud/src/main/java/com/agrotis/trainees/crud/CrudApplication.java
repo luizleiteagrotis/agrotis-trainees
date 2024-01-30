aa48cd01be851258fff82680427fd30eb62801f5
KayoRBC <kayo10br@gmail.com>
2024-01-30 18:23:44 -0300

    TP-139 refactor(Crud): adiciona dependencia do flyway

--- crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
+++ crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
@@ -1,9 +1,14 @@
 package com.agrotis.trainees.crud;
 
 import com.agrotis.trainees.crud.entity.NotaFiscalTipo;
+import com.agrotis.trainees.crud.entity.ParceiroNegocio;
+import com.agrotis.trainees.crud.repository.ParceiroNegocioRepository;
 import com.agrotis.trainees.crud.service.NotaFiscalTipoService;
+import com.agrotis.trainees.crud.service.ParceiroNegocioService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,10 +21,12 @@ public class CrudApplication implements CommandLineRunner {
 	private static final Logger LOG = LoggerFactory
 			.getLogger(CrudApplication.class);
 
-	private final NotaFiscalTipoService notaFiscalTipoService;
 
-	public CrudApplication(NotaFiscalTipoService notaFiscalTipoService) {
-		this.notaFiscalTipoService = notaFiscalTipoService;
+	private final ParceiroNegocioService parceiroNegocioService;
+	
+	@Autowired
+	public CrudApplication(ParceiroNegocioService parceiroNegocioService) {
+		this.parceiroNegocioService = parceiroNegocioService;
 	}
 
 	public static void main(String[] args) {
@@ -29,26 +36,15 @@ public class CrudApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args){
-		NotaFiscalTipo notaFiscalTipo = new NotaFiscalTipo();
-		notaFiscalTipo.setNome("nomeTeste");
-		NotaFiscalTipo notaFiscalTipo2 = notaFiscalTipoService.salvar(notaFiscalTipo);
-		LOG.info("id inserido: {}", notaFiscalTipo2.getId());
 
-		NotaFiscalTipo porId =  notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
-		LOG.info("Busca por id. Nome {} id {} ", porId.getNome(), porId.getId());
-
-		List<NotaFiscalTipo> todosSalvos = notaFiscalTipoService.listarTodos();
-		LOG.info("Salvos no total de {} tipos de notas", todosSalvos.size());
-
-		NotaFiscalTipo porNome = notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
-		porNome.setNome("nomeAlterado");
-		notaFiscalTipoService.salvar(porNome);
-		LOG.info("Busca por nome. Nome {} id {} ", porNome.getNome(), porNome.getId());
-
-		//notaFiscalTipoService.deletarPorId(porId.getId());
-		notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
-		 notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
-
-
+		////////////////////////////////////////
+		
+		ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
+		parceiroNegocio.setNome("nomeTeste");
+		parceiroNegocio.setInscricaoFiscal("inscricaoTeste");
+		parceiroNegocio.setEndereco("enderecoTeste");
+		parceiroNegocio.setTelefone("12345");
+		parceiroNegocio = parceiroNegocioService.salvar(parceiroNegocio);
+		LOG.info("id inserido: {}", parceiroNegocio.getId());
 	}
 }
---
+++ crud/src/main/resources/db/migration/V2__create_table_parceiro_de_negocio.sql
@@ -0,0 +1,7 @@
+CREATE TABLE parceiro_de_negocio (
+	id BIGINT PRIMARY KEY IDENTITY,
+	nome VARCHAR(50),
+	inscricao_fiscal VARCHAR(255),
+	endereco VARCHAR(100),
+	telefone VARCHAR(20)
+);
