d2e4ffaff79b9d0cdd6adffb02ddbc577c709d44
matheus.lourenco <matheuslourenco111@gmail.com>
2024-01-31 10:28:47 -0300

    TP-133 refactor(ParceiroNegocio) melhorias no método salvar aonde verifica se existe uma inscrição fiscal no banco e melhoria no método deletar aonde verifica se existe o id no banco

--- crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
+++ crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
@@ -1,11 +1,5 @@
 package com.agrotis.trainees.crud;
 
-import com.agrotis.trainees.crud.entity.NotaFiscalTipo;
-import com.agrotis.trainees.crud.entity.ParceiroNegocio;
-import com.agrotis.trainees.crud.service.NotaFiscalTipoService;
-import com.agrotis.trainees.crud.service.ParceiroNegocioService;
-
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,73 +8,86 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 
-import javax.validation.ConstraintViolationException;
+import com.agrotis.trainees.crud.entity.ParceiroNegocio;
+import com.agrotis.trainees.crud.service.NotaFiscalTipoService;
+import com.agrotis.trainees.crud.service.ParceiroNegocioService;
 
 @SpringBootApplication
 public class CrudApplication implements CommandLineRunner {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(CrudApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CrudApplication.class);
 
-	private final NotaFiscalTipoService notaFiscalTipoService;
-	private final ParceiroNegocioService parceiroNegocioService;
+    private final NotaFiscalTipoService notaFiscalTipoService;
+    private final ParceiroNegocioService parceiroNegocioService;
 
-	public CrudApplication(NotaFiscalTipoService notaFiscalTipoService, ParceiroNegocioService parceiroNegocioService) {
-		this.notaFiscalTipoService = notaFiscalTipoService;
-		this.parceiroNegocioService = parceiroNegocioService;
-	}
+    public CrudApplication(NotaFiscalTipoService notaFiscalTipoService, ParceiroNegocioService parceiroNegocioService) {
+        this.notaFiscalTipoService = notaFiscalTipoService;
+        this.parceiroNegocioService = parceiroNegocioService;
+    }
 
-	public static void main(String[] args) {
-		LOG.info("Iniciado com sucesso!");
-		SpringApplication.run(CrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        LOG.info("Iniciado com sucesso!");
+        SpringApplication.run(CrudApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args){
-		
+    @Override
+    public void run(String... args) {
+        try {
+            ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
+            parceiroNegocio.setNome("Hadassa e Tatiane Esportes Ltda");
+            parceiroNegocio.setInscricaoFiscal("11111704001134");
+            // parceiroNegocio.setEndereco("Moradias Bom Jesus");
+            parceiroNegocio.setTelefone("41992477204");
+            // Create
+            ParceiroNegocio parceiroNegocio2 = parceiroNegocioService.salvar(parceiroNegocio);
+            LOG.info("id inserio: {}", parceiroNegocio2.getId());
 
-		ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
-		parceiroNegocio.setNome("Heitor e Elias Pães e Doces Ltda");
-		parceiroNegocio.setInscricaoFiscal("00213412200035");
-		parceiroNegocio.setEndereco("Rua Barão de Cocais");
-		parceiroNegocio.setTelefone("11414168390");
-		
-		//Create
-		ParceiroNegocio parceiroNegocio2 = parceiroNegocioService.salvar(parceiroNegocio);
-		LOG.info("id inserio: {}", parceiroNegocio2.getId());
-	
-		//select
-		ParceiroNegocio porId = parceiroNegocioService.buscarPorId(parceiroNegocio2.getId());
-		LOG.info("Buscar por id. Nome {} id {}", porId.getNome(), porId.getId());
-		ParceiroNegocio porInscricaoFiscal = parceiroNegocioService.buscarPorInscricaoFiscal(parceiroNegocio2.getInscricaoFiscal());
-		LOG.info("Buscar por if. Nome {} inscricao fiscal {}", porId.getNome(), porInscricaoFiscal.getInscricaoFiscal());
-		List<ParceiroNegocio> listarTodos = parceiroNegocioService.listarTodos();
-		for(ParceiroNegocio parceiro : listarTodos) {
-			LOG.info("Buscar por todos. Nome {} inscricao fiscal {}", parceiro.getNome(), parceiro.getInscricaoFiscal());
-		}
-		
-//		ParceiroNegocio atualizar = parceiroNegocioService.atualizar("21212704001134", "Hadassa e Tatiane Esportes Ltda","00000704001134", "Moradias Bom Jesus","41992477204");
-//		LOG.info("Atualizado. Nome {} inscricao fiscal {}", atualizar.getNome(), atualizar.getInscricaoFiscal());
-//		
-		parceiroNegocioService.deletar(54);
-//		
-//		NotaFiscalTipo notaFiscalTipo = new NotaFiscalTipo();
-//		notaFiscalTipo.setNome("nomeTeste");
-//		NotaFiscalTipo notaFiscalTipo2 = notaFiscalTipoService.salvar(notaFiscalTipo);
-//		LOG.info("id inserido: {}", notaFiscalTipo2.getId());
-//		NotaFiscalTipo porIdNF =  notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
-//		LOG.info("Busca por id. Nome {} id {} ", porIdNF.getNome(), porIdNF.getId());
-//		List<NotaFiscalTipo> todosSalvos = notaFiscalTipoService.listarTodos();
-//		LOG.info("Salvos no total de {} tipos de notas", todosSalvos.size());	
-//		NotaFiscalTipo porNome = notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
-//		porNome.setNome("nomeAlterado");
-//		notaFiscalTipoService.salvar(porNome);
-//		LOG.info("Busca por nome. Nome {} id {} ", porNome.getNome(), porNome.getId());
-//
-//		//notaFiscalTipoService.deletarPorId(porId.getId());
-//		notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
-//		 notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
+            // select
+            ParceiroNegocio porId = parceiroNegocioService.buscarPorId(parceiroNegocio2.getId());
+            LOG.info("Buscar por id. Nome {} id {}", porId.getNome(), porId.getId());
+            ParceiroNegocio porInscricaoFiscal = parceiroNegocioService
+                            .buscarPorInscricaoFiscal(parceiroNegocio2.getInscricaoFiscal());
+            LOG.info("Buscar por if. Nome {} inscricao fiscal {}", porId.getNome(), porInscricaoFiscal.getInscricaoFiscal());
+            List<ParceiroNegocio> listarTodos = parceiroNegocioService.listarTodos();
+            for (ParceiroNegocio parceiro : listarTodos) {
+                LOG.info("Buscar por todos. Nome {} inscricao fiscal {}", parceiro.getNome(), parceiro.getInscricaoFiscal());
+            }
+        } catch (IllegalArgumentException exp) {
+            System.out.println("Houve um erro ao tentar salvar no banco");
+        }
+        ;
 
+        // ParceiroNegocio atualizar =
+        // parceiroNegocioService.atualizar("21212704001134", "Hadassa e Tatiane
+        // Esportes Ltda","00000704001134", "Moradias Bom Jesus","41992477204");
+        // LOG.info("Atualizado. Nome {} inscricao fiscal {}",
+        // atualizar.getNome(), atualizar.getInscricaoFiscal());
+        //
+        parceiroNegocioService.deletar(54);
+        //
 
-	}
+        // NotaFiscalTipo notaFiscalTipo = new NotaFiscalTipo();
+        // notaFiscalTipo.setNome("nomeTeste");
+        // NotaFiscalTipo notaFiscalTipo2 =
+        // notaFiscalTipoService.salvar(notaFiscalTipo);
+        // LOG.info("id inserido: {}", notaFiscalTipo2.getId());
+        // NotaFiscalTipo porIdNF =
+        // notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
+        // LOG.info("Busca por id. Nome {} id {} ", porIdNF.getNome(),
+        // porIdNF.getId());
+        // List<NotaFiscalTipo> todosSalvos =
+        // notaFiscalTipoService.listarTodos();
+        // LOG.info("Salvos no total de {} tipos de notas", todosSalvos.size());
+        // NotaFiscalTipo porNome =
+        // notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
+        // porNome.setNome("nomeAlterado");
+        // notaFiscalTipoService.salvar(porNome);
+        // LOG.info("Busca por nome. Nome {} id {} ", porNome.getNome(),
+        // porNome.getId());
+        //
+        // //notaFiscalTipoService.deletarPorId(porId.getId());
+        // notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
+        // notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
+
+    }
 }
--- crud/src/main/java/com/agrotis/trainees/crud/service/ParceiroNegocioService.java
+++ crud/src/main/java/com/agrotis/trainees/crud/service/ParceiroNegocioService.java
@@ -1,87 +1,89 @@
 package com.agrotis.trainees.crud.service;
 
-
-
-import java.util.List;
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.agrotis.trainees.crud.entity.ParceiroNegocio;
 import com.agrotis.trainees.crud.repository.PaceiroNegocioRepository;
 
-
 @Service
 public class ParceiroNegocioService {
-	
-	private static final Logger LOG = LoggerFactory
-			.getLogger(NotaFiscalTipoService.class);
-	
-	private final PaceiroNegocioRepository repository;
-	
-	public ParceiroNegocioService(PaceiroNegocioRepository repository) {
-		super();
-		this.repository = repository;
-	}
-	
-	public ParceiroNegocio salvar(ParceiroNegocio entidade) {
-		//TO-DO validar se a inscricao fiscal já está cadastrada
-		if(entidade.getTelefone() == null) {
-			return repository.save(entidade);
-		}
-		if(entidade.getInscricaoFiscal().length() > 14 || entidade.getInscricaoFiscal().length() < 14 || entidade.getTelefone().length() > 14) {
-			LOG.error("Falha de validação: Não foi possivel salvar no banco de dados.");
-	        throw new IllegalArgumentException("Invalid input data");
 
-		}
-		return repository.save(entidade);
-	}
-	
-	public ParceiroNegocio buscarPorId(Integer id) {
-		return repository.findById(id).orElseGet(()->{
-			LOG.error("Nota não encontrada para id {}.", id);
-			return null;
-		});
-	}
-	
-	public ParceiroNegocio buscarPorInscricaoFiscal(String inscricaoFiscal) {
-		return repository.findByInscricaoFiscal(inscricaoFiscal).orElseGet(()->{
-			LOG.error("Nota não encontrada para id {}.", inscricaoFiscal);
-			return null;
-		});
-	}
-	
-	
-	public List<ParceiroNegocio> listarTodos() {
-		return repository.findAll();
-	}
-	
-	
-	public ParceiroNegocio atualizar(String incricaoFiscal, String nome, String NovaIscricaoFiscal, String endereco, String telefone) {
+    private static final Logger LOG = LoggerFactory.getLogger(NotaFiscalTipoService.class);
 
-		
-		Optional<ParceiroNegocio> parceiroOptional = repository.findByInscricaoFiscal(incricaoFiscal);
+    private final PaceiroNegocioRepository repository;
 
-		if (parceiroOptional.isPresent()) {
-		    ParceiroNegocio parceiro = parceiroOptional.get();
-		    parceiro.setNome(nome);
-		    parceiro.setInscricaoFiscal(NovaIscricaoFiscal);
-		    parceiro.setEndereco(endereco);
-		    parceiro.setTelefone(telefone);
-		    return repository.save(parceiro);
-		}
-		return null; 
+    public ParceiroNegocioService(PaceiroNegocioRepository repository) {
+        super();
+        this.repository = repository;
+    }
 
-	}
-	
-	public void deletar(Integer id) {
-		repository.deleteById(id);
-		LOG.info("Deletado com sucesso");
-		
-	}
-	
-	
-	
+    public ParceiroNegocio salvar(ParceiroNegocio entidade) {
+
+        if (buscarPorInscricaoFiscal(entidade.getInscricaoFiscal()) != null) {
+            LOG.error("Falha ao salvar no banco: Inscrição fiscal já está cadastrada.");
+            throw new IllegalArgumentException("Inscrição fiscal já está cadastrada");
+
+        }
+        if (entidade.getTelefone() == null) {
+            return repository.save(entidade);
+        }
+        if (entidade.getInscricaoFiscal().length() > 14 || entidade.getInscricaoFiscal().length() < 14
+                        || entidade.getTelefone().length() > 14) {
+            LOG.error("Falha de validação: Não foi possivel salvar no banco de dados.");
+            throw new IllegalArgumentException("Dado de cadastro inválido");
+
+        }
+        return repository.save(entidade);
+    }
+
+    public ParceiroNegocio buscarPorId(Integer id) {
+        return repository.findById(id).orElseGet(() -> {
+            LOG.error("Nota não encontrada para id {}.", id);
+            return null;
+        });
+    }
+
+    public ParceiroNegocio buscarPorInscricaoFiscal(String inscricaoFiscal) {
+        return repository.findByInscricaoFiscal(inscricaoFiscal).orElseGet(() -> {
+            LOG.error("Nota não encontrada para id {}.", inscricaoFiscal);
+            return null;
+        });
+    }
+
+    public List<ParceiroNegocio> listarTodos() {
+        return repository.findAll();
+    }
+
+    public ParceiroNegocio atualizar(String incricaoFiscal, String nome, String NovaIscricaoFiscal, String endereco,
+                    String telefone) {
+
+        Optional<ParceiroNegocio> parceiroOptional = repository.findByInscricaoFiscal(incricaoFiscal);
+
+        if (parceiroOptional.isPresent()) {
+            ParceiroNegocio parceiro = parceiroOptional.get();
+            parceiro.setNome(nome);
+            parceiro.setInscricaoFiscal(NovaIscricaoFiscal);
+            parceiro.setEndereco(endereco);
+            parceiro.setTelefone(telefone);
+            return repository.save(parceiro);
+        }
+        return null;
+
+    }
+
+    public void deletar(Integer id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            LOG.info("Deletado com sucesso");
+        } else {
+            LOG.info("Este registro não existe");
+        }
+
+    }
+
 }
