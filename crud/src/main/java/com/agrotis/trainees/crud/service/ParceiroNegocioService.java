3b9ad91e8b2dcaa2df620da336f484bcd15e8e14
GabrielMoreira23 <gabriel.moreira@agrotis.com>
2024-01-31 10:24:35 -0300

    TP-140 feat(ParceiroNegocio): Implementação do metódo buscar todos

--- crud/src/main/java/com/agrotis/trainees/crud/service/ParceiroNegocioService.java
+++ crud/src/main/java/com/agrotis/trainees/crud/service/ParceiroNegocioService.java
@@ -1,5 +1,7 @@
 package com.agrotis.trainees.crud.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -34,6 +36,9 @@ public class ParceiroNegocioService {
 	public void deletarPorId(Integer id){
 		repository.deleteById(id);
 		LOG.info("Deletado com sucesso");
+	}
+	public List<ParceiroNegocio> listarTodos() {
+		return repository.findAll();
 	}
 
 	
