c1c130152349c7037d235057ffc7783f7cd5c92a
vitorpodanoski <vitor.podanoski@agrotis.com>
2024-02-01 13:18:35 -0300

    TP-145 feat(Produto): adiciona criação de produto

--- crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
+++ crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @SpringBootApplication
@@ -36,66 +39,40 @@ public class CrudApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args){
-		crudParceiroNegocio();
-		crudProduto();
-	}
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	public void run(String... args) {
 
-	private void crudParceiroNegocio() {
-	/*--------------------------
-	Inicio CREATE*/
-	ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
-	parceiroNegocio.setNome("SR Photo");
-	parceiroNegocio.setEndereco("Rua Arlindo Nogueira, 166, Centro - Curitiba, Paraná");
-	parceiroNegocio.setInscricaoFiscal("11789600040");
-	parceiroNegocio.setTelefone("4840046315");
-	ParceiroNegocio parceiroNegocio1 = parceiroNegocioService.salvar(parceiroNegocio);
-	LOG.info("id inserido: {}", parceiroNegocio1.getId());
-	
-	
-	/*--------------------------
-	Inicio READ*/
-	ParceiroNegocio porId =  parceiroNegocioService.buscarPorId(parceiroNegocio1.getId());
-	porId.setNome("VR Engenharia");
-	LOG.info("Busca por id. Nome {} id {} ", porId.getNome(), porId.getId());
-			
-	List<ParceiroNegocio> todosSalvos = parceiroNegocioService.listarTodos();
-	LOG.info("Salvos no total de {} parceiros", todosSalvos.size());
-	
-	
-	/*--------------------------
-	Inicio UPDATE*/
-	parceiroNegocioService.buscarPorNome(parceiroNegocio.getNome());
-	
-	//Insira o nome da empresa que voce quer alterar entre parenteses (depois da função "buscarPorNome");
-	ParceiroNegocio porNome = parceiroNegocioService.buscarPorNome(parceiroNegocio.getNome());
-	//Insira o novo nome;
-	porNome.setNome("SR Photo");
-	parceiroNegocioService.salvar(porNome);
-	LOG.info("Nome alterado: novo nome {}", porNome.getNome() );
-
-	
-	/*-------------------
-	Inicio DELETE
-	Insira o ID do parceiro que você quer deletar na linha abaixo*/
-	parceiroNegocioService.deletarPorId(4);
-	//Insira o nome do parceiro que você quer deletar na linha abaixo;
-	porId.setNome("VR Engenharia");
-}
-
-	private void crudProduto() {
-		ParceiroNegocio parceiroNegocio1 = new ParceiroNegocio();
-		parceiroNegocio1.setNome("Gaspar");
-		parceiroNegocio1.setInscricaoFiscal("43246546546");
-		parceiroNegocio1.setEndereco("Rua Ubaldino Amaral");
-		parceiroNegocio1.setTelefone("4132226849");
-		parceiroNegocio1 = parceiroNegocioService.salvar(parceiroNegocio1);
-		LOG.info("id inserido: {}", parceiroNegocio1.getId());
+		ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
+		parceiroNegocio.setNome("Flut");
+		parceiroNegocio.setInscricaoFiscal("5045648889231");
+		parceiroNegocio.setEndereco("Rua Eunice Prado, 5648");
+		parceiroNegocio.setTelefone("41998775223");
+		parceiroNegocioService.salvar(parceiroNegocio);
+		LOG.info("id inserido: {}", parceiroNegocio.getId());
+		
+		ParceiroNegocio buscaParceiroID = parceiroNegocioService.buscarPorId(parceiroNegocio.getId());
+		LOG.info("Busca por id. Nome {} id {} ", buscaParceiroID.getNome(), buscaParceiroID.getId());
+		
+		ParceiroNegocio buscaParceiroNome = parceiroNegocioService.buscarPorNome(parceiroNegocio.getNome());
+		LOG.info("Busca por nome. Nome {} id {} ", buscaParceiroNome.getNome(), buscaParceiroNome.getId());
+		
+		List<ParceiroNegocio> parceirosSalvos = parceiroNegocioService.listarTodos();
+		LOG.info("Salvos no total de {} parceiros de negocio.", parceirosSalvos.size());
+		LOG.info("produto:");
+		
 		Produto produto = new Produto();
-		produto.setNomeProduto("Semente");
-		produto.setDescricao("Semente de girassol");
-		produto.setFabricante(parceiroNegocio1);
-		produto = produtoService.salvar(produto);
+		ParceiroNegocio parceiroNegocio2 = new ParceiroNegocio();
+		parceiroNegocio2.setNome("Abc Company");
+		parceiroNegocio2.setInscricaoFiscal("715921524546");
+		parceiroNegocio2.setEndereco("Rua Saldanha Marinho, 50");
+		parceiroNegocio2.setTelefone("48997655468");
+		parceiroNegocioService.salvar(parceiroNegocio2);
+		LOG.info("id inserido: {}", parceiroNegocio2.getId());
+		produto.setDataFabricacao(LocalDate.parse("11/11/2021", DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		produto.setDataValidade(LocalDate.parse("11/11/2025", DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		produto.setDescricao("Morango");
+		produto.setFabricante(parceiroNegocio2);
+		produtoService.salvar(produto);
 		LOG.info("id inserido: {}", produto.getId());
 		
 	}
--- crud/src/main/java/com/agrotis/trainees/crud/entity/Produto.java
+++ crud/src/main/java/com/agrotis/trainees/crud/entity/Produto.java
@@ -1,5 +1,7 @@
 package com.agrotis.trainees.crud.entity;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,65 +13,62 @@ import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
-
 @Entity
-@Table(name = "produto") 
+@Table(name = "produto")
 public class Produto {
-	
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-    private String nomeProduto;
-    private String descricao;
 
-    @ManyToOne
-    @JoinColumn(name = "fabricante_id")
-    private ParceiroNegocio fabricante;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 
-    @Column(name = "data_fabricacao")
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    private LocalDate dataFabricacao;
+	private String descricao;
 
-    @Column(name = "data_validade")
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
+	@ManyToOne
+	@JoinColumn(name = "fabricante_id") 
+	private ParceiroNegocio fabricante;
+
+	@Column(name = "data_fabricacao")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataFabricacao;
+
+	@Column(name = "data_validade")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataValidade;
-	
-	public String getNomeProduto() {
-		return nomeProduto;
-	}
-	public void setNomeProduto(String nomeProduto) {
-		this.nomeProduto = nomeProduto;
-	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public ParceiroNegocio getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(ParceiroNegocio fabricante) {
+		this.fabricante = fabricante;
+	}
+
 	public LocalDate getDataFabricacao() {
 		return dataFabricacao;
 	}
+
 	public void setDataFabricacao(LocalDate dataFabricacao) {
 		this.dataFabricacao = dataFabricacao;
 	}
+
 	public LocalDate getDataValidade() {
 		return dataValidade;
 	}
+
 	public void setDataValidade(LocalDate dataValidade) {
 		this.dataValidade = dataValidade;
 	}
-	public Object getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-    public ParceiroNegocio getFabricante() {
-        return fabricante;
-    }
 
-    public void setFabricante(ParceiroNegocio fabricante) {
-        this.fabricante = fabricante;
-    }
+	public Integer getId() {
+		return id;
+	}
+
 }
