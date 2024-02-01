52681e6171d1b2f4983e083de67277b760198538
Gabriel <gabrielveiga54@gmail.com>
2024-02-01 11:14:39 -0300

    TP-144 feat(CrudApplication): implementa estrutura de desvios para o usuário conseguir selecionar qual entidade ele quer usar o CRUD

--- crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
+++ crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.agrotis.trainees.crud.entity.NotaFiscalTipo;
 import com.agrotis.trainees.crud.entity.ParceiroNegocio;
 import com.agrotis.trainees.crud.entity.Produto;
 import com.agrotis.trainees.crud.service.NotaFiscalTipoService;
@@ -43,96 +45,114 @@ public class CrudApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws ParseException {
 
-//		NotaFiscalTipo notaFiscalTipo = new NotaFiscalTipo();
-//		notaFiscalTipo.setNome("nomeTeste5");
-//		NotaFiscalTipo notaFiscalTipo2 = notaFiscalTipoService.salvar(notaFiscalTipo);
-//		LOG.info("id inserido: {}", notaFiscalTipo2.getId());
-//
-//		NotaFiscalTipo porId =  notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
-//		LOG.info("Busca por id. Nome {} id {} ", porId.getNome(), porId.getId());
-//
-//		List<NotaFiscalTipo> todosSalvos = notaFiscalTipoService.listarTodos();
-//		LOG.info("Salvos no total de {} tipos de notas", todosSalvos.size());
-//
-//		NotaFiscalTipo porNome = notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
-//		porNome.setNome("nomeAlterado");
-//		notaFiscalTipoService.salvar(porNome);
-//		LOG.info("Busca por nome. Nome {} id {} ", porNome.getNome(), porNome.getId());
-//
-//		//notaFiscalTipoService.deletarPorId(porId.getId());
-//		notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
-//		 notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
-//
-//		
-//		
-		ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
-
-		parceiroNegocio.setNome("Empresa de adubo");
-		parceiroNegocio.setInscricaoFiscal("020924");
-		parceiroNegocio.setEndereco("Sao Paulo, rua cerveja, 15");
-		parceiroNegocio.setTelefone("11 9 5454 5454");
-	
-
-		ParceiroNegocio parceiroNegocio2 = parceiroNegocioService.salvar(parceiroNegocio);
-		LOG.info("id inserido: {}", parceiroNegocio2.getId());
-//		
-//		
-//		ParceiroNegocio parceiroPorId = parceiroNegocioService.buscarPorId(parceiroNegocio2.getId()); 
-//		LOG.info("Busca por id. Nome {} Inscricao Fiscal {} Endereco {} Telefone {} id {}", 
-//				parceiroPorId.getNome(), parceiroPorId.getInscricaoFiscal(), parceiroPorId.getEndereco(), parceiroPorId.getTelefone(), parceiroPorId.getId());
-//		
-//		ParceiroNegocio parceiroPorNome = parceiroNegocioService.buscarPorNome(parceiroNegocio2.getNome());
-//		LOG.info("Busca por nome. Nome {} id {}", parceiroPorNome.getNome(), parceiroPorNome.getId());
-//		
-//		List<ParceiroNegocio> todosParceiros = parceiroNegocioService.listarTodos();
-//		LOG.info("Salvos no total de {} tipos de parceiros", todosParceiros.size() );
-//		
-//		
-//		ParceiroNegocio parceiroPorNome2 = parceiroNegocioService.buscarPorNome(parceiroNegocio2.getNome());
-//		parceiroPorNome2.setNome("Racao de Campo Largo");
-//		parceiroPorNome2.setEndereco("Campo Largo, Bom jesus, 333");
-//		parceiroPorNome2.setTelefone("41 9 2222 1111");
-//		parceiroNegocioService.salvar(parceiroPorNome2);
-//		LOG.info("Nome, endereco e telefone atualizado com sucesso, novo nome: {}, novo endereco: {}, novo telefone: {} ", 
-//				parceiroPorNome2.getNome(), parceiroPorNome2.getEndereco(), parceiroPorNome2.getTelefone() );
-
-//		parceiroNegocioService.deletarPorId(parceiroPorId.getId());
-
-		Produto produto = new Produto();
-		produto.setDescricao("Adubo");
-		produto.setParceiroNegocio(parceiroNegocio);
+		Scanner scan = new Scanner(System.in);
+		System.out.println(
+				"Digite o número correspondente para mexer com o CRUD da nota fiscal, parceiro de negócio ou produto: 1 2 3");
+		int escolha = scan.nextInt();
 		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-		
-		Date fabricacaoDate = dateFormat.parse("03-07-2008");
-		produto.setDataFabricacao(fabricacaoDate);
-		
-		Date validaDate = dateFormat.parse("08-08-2018");
-		produto.setDataValidade(validaDate);
-		
-		Produto produto2 = produtoService.salvar(produto);
 
-		Produto produtoPorId = produtoService.buscarPorId(produto2.getId());
-		LOG.info("Busca por id. Descrição {} Data Fabricação {} Data Validade {} Nome da empresa parceira {}",
-				produtoPorId.getDescricao(), produtoPorId.getDataFabricacao(), produtoPorId.getDataValidade(), produtoPorId.getParceiroNegocio() );
+		if (escolha == 1) {
+			NotaFiscalTipo notaFiscalTipo = new NotaFiscalTipo();
 
-//		Produto produtoPorDescricao = produtoService.buscarPorDescricao(produto2.getDescricao());
-//		LOG.info("Busca por descricao. Descrição {} Data Fabricação {} Data Validade {} Nome da empresa parceira {}",
-//				produtoPorDescricao.getDescricao(), produtoPorDescricao.getDataFabricacao(), produtoPorDescricao.getDataValidade(), produtoPorDescricao.getParceiroNegocio() );
+			notaFiscalTipo.setNome("nomeTeste5");
+			NotaFiscalTipo notaFiscalTipo2 = notaFiscalTipoService.salvar(notaFiscalTipo);
+			LOG.info("id inserido: {}", notaFiscalTipo2.getId());
 
-//		List<Produto> todosProdutos = produtoService.listarTodos();
-//		LOG.info("Salvos no total de {} tipos de produtos", todosProdutos.size());
+			NotaFiscalTipo porId = notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
+			LOG.info("Busca por id. Nome {} id {} ", porId.getNome(), porId.getId());
 
-//		Produto produtoPorDescricao2 = produtoService.buscarPorDescricao(produto2.getDescricao());
-//		produtoPorDescricao2.setDescricao("Nova ração importada");
-//		Date novaValidadeDate = dateFormat.parse("03-07-2016");
-//		produtoPorDescricao2.setDataValidade(novaValidadeDate);
-//		produtoPorDescricao2.setParceiroNegocio(parceiroNegocio2);
-//		produtoService.salvar(produtoPorDescricao2);
-//		System.out.println(produtoPorDescricao2.getParceiroNegocio());
-//		LOG.info("Nova descrição {}, nova data validade {} e novo parceiro {}: ", produtoPorDescricao2.getDescricao(), produtoPorDescricao2.getDataValidade(), produtoPorDescricao2.getParceiroNegocio());
+			List<NotaFiscalTipo> todosSalvos = notaFiscalTipoService.listarTodos();
+			LOG.info("Salvos no total de {} tipos de notas", todosSalvos.size());
 
-		produtoService.deletarPorId(produtoPorId.getId());
+			NotaFiscalTipo porNome = notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
+			porNome.setNome("nomeAlterado");
+			notaFiscalTipoService.salvar(porNome);
+			LOG.info("Busca por nome. Nome {} id {} ", porNome.getNome(), porNome.getId());
+
+			// notaFiscalTipoService.deletarPorId(porId.getId());
+			notaFiscalTipoService.buscarPorId(notaFiscalTipo2.getId());
+			notaFiscalTipoService.buscarPorNome(notaFiscalTipo.getNome());
+
+		}
+
+		if (escolha == 2) {
+			ParceiroNegocio parceiroNegocio = new ParceiroNegocio();
+
+			parceiroNegocio.setNome("Empresa de citrus");
+			parceiroNegocio.setInscricaoFiscal("020924");
+			parceiroNegocio.setEndereco("Sao Paulo, rua cerveja, 15");
+			parceiroNegocio.setTelefone("11 9 5454 5454");
+
+			ParceiroNegocio parceiroNegocio2 = parceiroNegocioService.salvar(parceiroNegocio);
+			LOG.info("id inserido: {}", parceiroNegocio2.getId());
+
+			ParceiroNegocio parceiroPorId = parceiroNegocioService.buscarPorId(parceiroNegocio2.getId());
+			LOG.info("Busca por id. Nome {} Inscricao Fiscal {} Endereco {} Telefone {} id {}", parceiroPorId.getNome(),
+					parceiroPorId.getInscricaoFiscal(), parceiroPorId.getEndereco(), parceiroPorId.getTelefone(),
+					parceiroPorId.getId());
+
+			ParceiroNegocio parceiroPorNome = parceiroNegocioService.buscarPorNome(parceiroNegocio2.getNome());
+			LOG.info("Busca por nome. Nome {} id {}", parceiroPorNome.getNome(), parceiroPorNome.getId());
+
+			List<ParceiroNegocio> todosParceiros = parceiroNegocioService.listarTodos();
+			LOG.info("Salvos no total de {} tipos de parceiros", todosParceiros.size());
+
+			ParceiroNegocio parceiroPorNome2 = parceiroNegocioService.buscarPorNome(parceiroNegocio2.getNome());
+			parceiroPorNome2.setNome("Empresa de citrus e ração");
+			parceiroPorNome2.setEndereco("Curitiba, Centro, 333");
+			parceiroPorNome2.setTelefone("41 9 2222 1111");
+			parceiroNegocioService.salvar(parceiroPorNome2);
+			LOG.info(
+					"Nome, endereco e telefone atualizado com sucesso, novo nome: {}, novo endereco: {}, novo telefone: {} ",
+					parceiroPorNome2.getNome(), parceiroPorNome2.getEndereco(), parceiroPorNome2.getTelefone());
+
+			// parceiroNegocioService.deletarPorId(parceiroPorId.getId());
+		}
+
+		if (escolha == 3) {
+			Produto produto = new Produto();
+			
+			ParceiroNegocio fabricante = parceiroNegocioService.buscarPorId(157);
+			produto.setDescricao("Citrus e ração");
+			produto.setParceiroNegocio(fabricante);
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+			Date fabricacaoDate = dateFormat.parse("03-07-2008");
+			produto.setDataFabricacao(fabricacaoDate);
+
+			Date validaDate = dateFormat.parse("08-08-2018");
+			produto.setDataValidade(validaDate);
+
+			Produto produto2 = produtoService.salvar(produto);
+
+			Produto produtoPorId = produtoService.buscarPorId(produto2.getId());
+			LOG.info("Busca por id. Descrição {} Data Fabricação {} Data Validade {} Nome da empresa parceira {}",
+					produtoPorId.getDescricao(), produtoPorId.getDataFabricacao(), produtoPorId.getDataValidade(),
+					produtoPorId.getParceiroNegocio());
+
+			Produto produtoPorDescricao = produtoService.buscarPorDescricao(produto2.getDescricao());
+			LOG.info(
+					"Busca por descricao. Descrição {} Data Fabricação {} Data Validade {} Nome da empresa parceira {}",
+					produtoPorDescricao.getDescricao(), produtoPorDescricao.getDataFabricacao(),
+					produtoPorDescricao.getDataValidade(), produtoPorDescricao.getParceiroNegocio());
+
+			List<Produto> todosProdutos = produtoService.listarTodos();
+			LOG.info("Salvos no total de {} tipos de produtos", todosProdutos.size());
+
+			Produto produtoPorDescricao2 = produtoService.buscarPorDescricao(produto2.getDescricao());
+			produtoPorDescricao2.setDescricao("citrus e ração importados");
+			Date novaValidadeDate = dateFormat.parse("03-07-2016");
+			produtoPorDescricao2.setDataValidade(novaValidadeDate);
+//			produtoPorDescricao2.setParceiroNegocio(fabricante);
+			produtoService.salvar(produtoPorDescricao2);
+			System.out.println(produtoPorDescricao2.getParceiroNegocio());
+			LOG.info("Nova descrição {}, nova data validade {} e novo parceiro {}: ",
+					produtoPorDescricao2.getDescricao(), produtoPorDescricao2.getDataValidade(),
+					produtoPorDescricao2.getParceiroNegocio());
+
+//		produtoService.deletarPorId(produtoPorId.getId());
+		}
 
 	}
 }
