bbe721d6c1d121451385f929060520aca1001e96
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2024-02-05 16:24:55 -0300

    wip(Teste): teste

--- crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
+++ crud/src/main/java/com/agrotis/trainees/crud/CrudApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CrudApplication.class, args);
+		SpringApplication.run(CrudApplication.class, args); //TODO remover comentário
 	}
 }
