b23fe6cf4da7592b14cdd87249b8de8c50a77ed8
Anderson <andersondesouzalopes69@gmail.com>
2024-02-06 15:50:15 -0300

    TP-176 feat(ParceiroNegocio): refatoração do método salvar, deletarPorId  , buscaPorNome. Adicionado o tratamento para caso a buscaPorId não retorne uma entidade

--- crud/src/main/java/com/agrotis/trainees/crud/service/ParceiroNegocioService.java
+++ crud/src/main/java/com/agrotis/trainees/crud/service/ParceiroNegocioService.java
@@ -8,51 +8,58 @@ import org.springframework.stereotype.Service;
 
 import com.agrotis.trainees.crud.entity.ParceiroNegocio;
 import com.agrotis.trainees.crud.repository.ParceiroNegocioRepository;
+import com.agrotis.trainees.crud.service.exceptions.EntidadeNaoEncontradaException;
 
 @Service
 public class ParceiroNegocioService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ParceiroNegocioService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ParceiroNegocioService.class);
 
-	private final ParceiroNegocioRepository repository;
+    private final ParceiroNegocioRepository repository;
 
-	public ParceiroNegocioService(ParceiroNegocioRepository repository) {
-		this.repository = repository;
-	}
+    public ParceiroNegocioService(ParceiroNegocioRepository repository) {
+        this.repository = repository;
+    }
 
-	public ParceiroNegocio salvar(ParceiroNegocio negocio) {
-		return repository.save(negocio);
-	}
+    public ParceiroNegocio salvar(ParceiroNegocio negocio) {
+        return repository.save(negocio);
+    }
 
-	public ParceiroNegocio buscarPorId(Integer id) {
-		return repository.findById(id).orElseGet(() -> {
-			LOG.info("Não foi possível buscar pelo id {}", id);
-			return null;
-		});
-	}
+    public ParceiroNegocio buscarPorId(Integer id) {
+        return repository.findById(id)
+                        .orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada com o ID: " + id));
+    }
 
-	public ParceiroNegocio buscarPorNome(String nome) {
-		return repository.findByNome(nome).orElseGet(() -> {
-			LOG.info("Não foi possível encontrar o parceiro de negocio pelo nome {}", nome);
-			return null;
-		});
-	}
+    public ParceiroNegocio buscarPorNome(String nome) {
+        return repository.findByNome(nome).orElseThrow(() -> {
+            LOG.info("Não foi possível encontrar o parceiro de negócio pelo nome {}", nome);
+            return new EntidadeNaoEncontradaException("Parceiro de negócio com o nome '" + nome + "' não encontrado");
+        });
+    }
 
-	public void deletarPorId(Integer id) {
-		repository.deleteById(id);
-		LOG.info("Deletado com sucesso");
-	}
+    public void deletarPorId(Integer id) {
+        repository.findById(id).map(entidade -> {
+            repository.deleteById(id);
+            LOG.info("Deletado com sucesso");
+            return entidade;
+        }).orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade com o ID " + id + " não encontrada"));
+    }
 
-	public List<ParceiroNegocio> listarTodos() {
-		return repository.findAll();
-	}
+    public List<ParceiroNegocio> listarTodos() {
+        return repository.findAll();
+    }
 
-	public ParceiroNegocio update(Integer id, ParceiroNegocio negocio) {
-		ParceiroNegocio byId = repository.findById(id).orElseGet(() -> {
-			LOG.info("Não foi possível encontrar o parceiro de negocio pelo ID {}", id);
-			return null;
-		});
-		return repository.save(byId);
-	}
+    public ParceiroNegocio salvar(Integer id, ParceiroNegocio negocio) {
+        return repository.findById(id).map(parceiroExistente -> {
+            parceiroExistente.setNome(negocio.getNome());
+            parceiroExistente.setInscricaoFiscal(negocio.getInscricaoFiscal());
+            parceiroExistente.setEndereco(negocio.getEndereco());
+            parceiroExistente.setTelefone(negocio.getTelefone());
+            return repository.save(parceiroExistente);
+        }).orElseThrow(() -> {
+            LOG.info("Não foi possível encontrar o parceiro de negócio pelo ID {}", id);
+            return new EntidadeNaoEncontradaException("Parceiro de negócio com o ID " + id + " não encontrado");
+        });
+    }
 
 }
