eed9710929284564ceb8e42c8b252ac32f652874
Gabriel <gabrielveiga54@gmail.com>
2024-02-06 16:25:36 -0300

    TP-177 feat(QuantidadeInsuficienteException): implementa a exception que é lançada caso a validação da quantidade de itens que saem do estoque seja maior que a quantidade de produtos que existem

---
+++ crud/src/main/java/com/agrotis/trainees/crud/exception/QuantidadeInsuficienteException.java
@@ -0,0 +1,7 @@
+package com.agrotis.trainees.crud.exception;
+
+public class QuantidadeInsuficienteException extends Exception {
+    public QuantidadeInsuficienteException(String message) {
+        super(message);
+    }
+}
