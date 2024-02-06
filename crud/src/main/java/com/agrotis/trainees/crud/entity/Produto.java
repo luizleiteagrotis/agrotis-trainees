a1b69e9ab11dc9e06fbbc97725571bd3e8525cdb
KayoRBC <kayo10br@gmail.com>
2024-02-06 17:10:10 -0300

    TP-182 fix(Produto): retira @PastOrPresent de data de validade

--- crud/src/main/java/com/agrotis/trainees/crud/entity/Produto.java
+++ crud/src/main/java/com/agrotis/trainees/crud/entity/Produto.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
@@ -40,7 +39,6 @@ public class Produto {
 	private LocalDate dataFabricacao;
 	
 	@NotNull
-	@PastOrPresent
 	@Column(name = "data_validade")
 	private LocalDate dataValidade;
 	
