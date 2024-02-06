d9cd540c602052c06271959923f86f4dd2bd2f0b
Gabriel <gabrielveiga54@gmail.com>
2024-02-06 16:13:46 -0300

    arruma a identação dos comandos SQL para 4 espaços

--- crud/src/main/resources/sql/item_nota_fiscal.sql
+++ crud/src/main/resources/sql/item_nota_fiscal.sql
@@ -1,9 +1,9 @@
 CREATE TABLE item_nota_fiscal (
-id INT NOT NULL,
-id_produto INT NOT NULL,
-quantidade INT NOT NULL,
-valor_unitario DECIMAL(10, 2) NOT NULL,
-valor_total DECIMAL(10, 2) NOT NULL,
-CONSTRAINT pk_item_nota_fiscal PRIMARY KEY(id),
-CONSTRAINT fk_item_fiscal_produto FOREIGN KEY (id_produto) REFERENCES produto,
+    id INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    valor_unitario DECIMAL(10, 2) NOT NULL,
+    valor_total DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT pk_item_nota_fiscal PRIMARY KEY(id),
+    CONSTRAINT fk_item_fiscal_produto FOREIGN KEY (id_produto) REFERENCES produto,
 );
