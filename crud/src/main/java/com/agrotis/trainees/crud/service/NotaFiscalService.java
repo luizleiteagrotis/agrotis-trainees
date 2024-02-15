8e3d23f750f8a55044e01a10741e4c4f25229793
Willianr7 <Rafael.net@hotmail.com.br>
2024-02-15 16:41:36 -0300

    TP-204: Implementacao dos metodos converteParaEntidade e converteParaDto em NotaFiscalService.

--- crud/src/main/java/com/agrotis/trainees/crud/service/NotaFiscalService.java
+++ crud/src/main/java/com/agrotis/trainees/crud/service/NotaFiscalService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import com.agrotis.trainees.crud.dto.NotaFiscalDto;
 import com.agrotis.trainees.crud.entity.NotaFiscal;
 import com.agrotis.trainees.crud.repository.NotaFiscalRepository;
 import com.agrotis.trainees.crud.service.exceptions.EntidadeNaoEncontradaException;
@@ -46,6 +47,31 @@ public class NotaFiscalService {
     public void deletarPorId(Integer id) {
         repository.deleteById(id);
         LOG.info("Deletado com sucesso!");
+    }
+
+    public static NotaFiscal converteParaEntidade(NotaFiscalDto dto) {
+        NotaFiscal entidade = new NotaFiscal();
+
+        entidade.setData(dto.getData());
+        entidade.setNumero(dto.getNumero());
+        entidade.setTipo(dto.getTipo());
+        entidade.setParceiroNegocio(dto.getParceiroNegocio());
+        entidade.setValorTotal(dto.getValorTotal());
+
+        return entidade;
+    }
+
+    public static NotaFiscalDto converteParaDto(NotaFiscal entidade) {
+        NotaFiscalDto dto = new NotaFiscalDto();
+
+        dto.setId(entidade.getId());
+        dto.setData(entidade.getData());
+        dto.setNumero(dto.getNumero());
+        dto.setTipo(entidade.getTipo());
+        dto.setParceiroNegocio(dto.getParceiroNegocio());
+        dto.setValorTotal(dto.getValorTotal());
+
+        return dto;
     }
 
 }
