9c46efcc4863963ff17fc020c0d23d06a302ef6a
KayoRBC <kayo10br@gmail.com>
2024-02-16 14:11:41 -0300

    TP-209 refactor(ItemNota): deleta metodos getValorTotal e getQuantidade de ItemNotaRepository

--- crud/src/main/java/com/agrotis/trainees/crud/repository/item/ItemNotaRepository.java
+++ crud/src/main/java/com/agrotis/trainees/crud/repository/item/ItemNotaRepository.java
@@ -1,11 +1,8 @@
 package com.agrotis.trainees.crud.repository.item;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.agrotis.trainees.crud.entity.CabecalhoNota;
 import com.agrotis.trainees.crud.entity.ItemNota;
 import com.agrotis.trainees.crud.repository.wrapper.JpaRepositoryWrapper;
 
@@ -22,15 +19,5 @@ public class ItemNotaRepository extends JpaRepositoryWrapper<
 	public boolean existe(ItemNota item) {
 		if (item.getId() == null) return false;
 		return REPOSITORY.existsById(item.getId());
-	}
-	
-	public BigDecimal getValorTotal(Long idItem) {
-		ItemNotaJpaRepository repository = (ItemNotaJpaRepository) REPOSITORY;
-		return repository.getValorTotal(idItem);
-	}
-	
-	public Integer getQuantidade(Long idItem) {
-		ItemNotaJpaRepository repository = (ItemNotaJpaRepository) REPOSITORY;
-		return repository.getQuantidade(idItem);
 	}
 }
