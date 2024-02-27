d96eb10b359c8f61c79009b05dbcf61894344909
Gabriel <gabrielveiga54@gmail.com>
2024-02-27 17:07:20 -0300

    TP-214 refactor(NotaFiscal): melhora os testes da service NotaFiscal

--- crud/src/main/java/com/agrotis/trainees/crud/service/NotaFiscalService.java
+++ crud/src/main/java/com/agrotis/trainees/crud/service/NotaFiscalService.java
@@ -71,6 +71,7 @@ public class NotaFiscalService {
     }
 
     public void deletarPorId(Integer id) {
+        buscarPorId(id);
         repository.deleteById(id);
     }
 }
--- crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalServiceTest.java
+++ crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalServiceTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -14,14 +17,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import com.agrotis.trainees.crud.dto.NotaFiscalDto;
+import com.agrotis.trainees.crud.entity.ItemNotaFiscal;
 import com.agrotis.trainees.crud.entity.NotaFiscal;
 import com.agrotis.trainees.crud.entity.NotaFiscalTipo;
+import com.agrotis.trainees.crud.entity.ParceiroNegocio;
 import com.agrotis.trainees.crud.exception.NotaFiscalCabecalhoNaoEncontradaException;
 import com.agrotis.trainees.crud.exception.NotaFiscalDuplicadaException;
 import com.agrotis.trainees.crud.repository.NotaFiscalRepository;
@@ -29,8 +35,13 @@ import com.agrotis.trainees.crud.utils.NotaFiscalDTOMapper;
 
 public class NotaFiscalServiceTest {
 
+    private final Integer ID = 1;
+    private final LocalDate DATA = LocalDate.now();
     private final String NUMERO = "123456";
-    private final String NOME = "ENTRADA";
+    private final BigDecimal VALORTOTAL = new BigDecimal(0);
+    private final List<ItemNotaFiscal> ITENS = new ArrayList<>();
+
+    private final String TIPO = "ENTRADA";
 
     @Mock
     private NotaFiscalRepository repository;
@@ -38,121 +49,105 @@ public class NotaFiscalServiceTest {
     @Mock
     private NotaFiscalDTOMapper mapper;
 
+    @Mock
+    private ParceiroNegocio fabricante;
+
+    @Mock
+    private NotaFiscalTipo notaFiscalTipo;
+
     @InjectMocks
     private NotaFiscalService service;
+
+    private NotaFiscal notaFiscal;
+
+    private NotaFiscalDto notaFiscalDto;
+
+    private Optional<NotaFiscal> notaFiscalOpcional;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        inicializarNotaFiscal();
     }
 
     @Test
     public void deveriaNaoSalvarNotaFiscalExistente() {
-        NotaFiscal notaFiscal = new NotaFiscal();
-        NotaFiscalDto notaFiscalDto = new NotaFiscalDto();
-        when(mapper.converterParaEntidade(notaFiscalDto)).thenReturn(notaFiscal);
         when(repository.existsByNumeroAndNotaFiscalTipo(any(), any())).thenReturn(true);
 
         Exception excecao = assertThrows(NotaFiscalDuplicadaException.class, () -> {
             service.salvar(notaFiscalDto);
         });
-
+        
+        assertEquals(NotaFiscalDuplicadaException.class, excecao.getClass());
         assertEquals("Nota fiscal já existe", excecao.getMessage());
     }
 
     @Test
     public void deveSalvarNotaFiscal() {
-        NotaFiscal notaFiscal = new NotaFiscal();
-        NotaFiscalDto notaFiscalDto = new NotaFiscalDto();
-        when(mapper.converterParaEntidade(notaFiscalDto)).thenReturn(notaFiscal);
         when(repository.existsByNumeroAndNotaFiscalTipo(any(), any())).thenReturn(false);
-        when(repository.save(notaFiscal)).thenReturn(notaFiscal);
-
-        when(service.salvar(notaFiscalDto)).thenReturn(notaFiscalDto);
+        when(repository.save(any())).thenReturn(notaFiscal);
+        
+        NotaFiscalDto notaSalva = service.salvar(notaFiscalDto);
+        
         verify(repository, times(1)).save(notaFiscal);
-        NotaFiscalDto notaFiscalDtoSalva = service.salvar(notaFiscalDto);
-        assertEquals(notaFiscalDto, notaFiscalDtoSalva);
+        assertNotNull(notaSalva);
+        assertEquals(NotaFiscalDto.class, notaSalva.getClass());
+        assertEquals(ID, notaSalva.getId());
     }
 
     @Test
     public void deveriaNaoAtualizarNotaFiscal() {
-        NotaFiscal notaFiscal = new NotaFiscal();
-        notaFiscal.setId(1000);
-        notaFiscal.setNumero(NUMERO);
-        NotaFiscalTipo tipo = new NotaFiscalTipo();
-        tipo.setNome(NOME);
-        notaFiscal.setNotaFiscalTipo(tipo);
-
-        NotaFiscalDto notaFiscalDto = new NotaFiscalDto();
-        notaFiscalDto.setId(notaFiscal.getId());
-        notaFiscalDto.setNumero(notaFiscal.getNumero());
-        notaFiscal.setNotaFiscalTipo(notaFiscal.getNotaFiscalTipo());
-
-        when(mapper.converterParaEntidade(notaFiscalDto)).thenReturn(notaFiscal);
-        when(repository.existsByNumeroAndNotaFiscalTipoAndIdNot(notaFiscal.getNumero(), notaFiscal.getNotaFiscalTipo(),
-                        notaFiscal.getId())).thenReturn(true);
-
+        when(repository.existsByNumeroAndNotaFiscalTipoAndIdNot(anyString(), any(),
+                        anyInt())).thenReturn(true);
+        
         Exception excecao = assertThrows(NotaFiscalDuplicadaException.class, () -> {
             service.atualizar(notaFiscalDto);
         });
 
-        assertEquals("Nota com o numero e o tipo já existe: " + NUMERO + " " + NOME, excecao.getMessage());
+         assertEquals("Nota com o numero e o tipo já existe: " + NUMERO + " " + TIPO, excecao.getMessage());
     }
 
     @Test
     public void deveAtualizarNotaFiscal() {
-        NotaFiscal notaFiscal = new NotaFiscal();
-        notaFiscal.setId(1000);
-        notaFiscal.setNumero(NUMERO);
-        NotaFiscalTipo tipo = new NotaFiscalTipo();
-        tipo.setNome(NOME);
-        notaFiscal.setNotaFiscalTipo(tipo);
+        when(repository.existsByNumeroAndNotaFiscalTipoAndIdNot(anyString(), any(),
+                        anyInt())).thenReturn(false);
+        when(repository.save(any())).thenReturn(notaFiscal);
 
-        NotaFiscalDto notaFiscalDto = new NotaFiscalDto();
-        notaFiscalDto.setId(notaFiscal.getId());
-        notaFiscalDto.setNumero(notaFiscal.getNumero());
-        notaFiscal.setNotaFiscalTipo(notaFiscal.getNotaFiscalTipo());
+        NotaFiscalDto notaFiscalAtualizada = service.atualizar(notaFiscalDto);
 
-        when(mapper.converterParaEntidade(notaFiscalDto)).thenReturn(notaFiscal);
-        when(repository.existsByNumeroAndNotaFiscalTipoAndIdNot(notaFiscal.getNumero(), notaFiscal.getNotaFiscalTipo(),
-                        notaFiscal.getId())).thenReturn(false);
-        when(repository.save(notaFiscal)).thenReturn(notaFiscal);
-
-        when(service.atualizar(notaFiscalDto)).thenReturn(notaFiscalDto);
         verify(repository, times(1)).save(notaFiscal);
-        NotaFiscalDto notaFiscalDtoSalva = service.atualizar(notaFiscalDto);
-        assertEquals(notaFiscalDto, notaFiscalDtoSalva);
+        assertNotNull(notaFiscalAtualizada);
+        assertEquals(NotaFiscalDto.class, notaFiscalAtualizada.getClass());
+        assertEquals(ID, notaFiscalAtualizada.getId());
     }
 
     @Test
     public void naoDeveriaBuscarPorIdInexistente() {
-        when(repository.findById(1000)).thenReturn(Optional.empty());
+        when(repository.findById(anyInt())).thenReturn(Optional.empty());
         
         Exception excecao = assertThrows(NotaFiscalCabecalhoNaoEncontradaException.class, () -> {
-            service.buscarPorId(1000);
+            service.buscarPorId(ID);
         });
         
-        assertEquals("Nota Fiscal não encontrada para o id " + 1000, excecao.getMessage());
+        assertEquals(NotaFiscalCabecalhoNaoEncontradaException.class, excecao.getClass());
+        assertEquals("Nota Fiscal não encontrada para o id " + ID, excecao.getMessage());
     }
 
     @Test
-    public void deveBuscarPorId() {
-        NotaFiscal notaFiscal = new NotaFiscal();
-        notaFiscal.setId(1000);
-        NotaFiscalDto notaFiscalDto = new NotaFiscalDto();
-        when(mapper.converterParaDto(notaFiscal)).thenReturn(notaFiscalDto);
-        when(repository.findById(1000)).thenReturn(Optional.of(notaFiscal));
-        notaFiscalDto.setId(notaFiscal.getId());
+    public void deveBuscarPorIdExistente() {
+        when(repository.findById(anyInt())).thenReturn(notaFiscalOpcional);
 
-        when(service.buscarPorId(notaFiscalDto.getId())).thenReturn(notaFiscalDto);
-        verify(repository, times(1)).findById(1000);
-        NotaFiscalDto notaAchada = service.buscarPorId(1000);
-        assertEquals(notaFiscal.getId(), notaAchada.getId());
+        NotaFiscalDto notaAchada = service.buscarPorId(ID);
+
+        assertNotNull(notaAchada);
+        assertEquals(NotaFiscalDto.class, notaAchada.getClass());
+        assertEquals(ID, notaAchada.getId());
+        verify(repository, times(1)).findById(ID);
     }
 
     @Test
-    public void naoDeveriaBuscarPorNomeInexistente() {
-        when(repository.findAllByNumero(NUMERO)).thenReturn(new ArrayList<>());
+    public void naoDeveriaBuscarPorNumeroInexistente() {
+        when(repository.findAllByNumero(anyString())).thenReturn(new ArrayList<>());
         
         Exception excecao = assertThrows(NotaFiscalCabecalhoNaoEncontradaException.class, () -> { 
             service.buscarPorNumero(NUMERO);
@@ -163,29 +158,76 @@ public class NotaFiscalServiceTest {
 
     @Test
     public void deveBuscarNotaFiscalPorNumero() {
-        List<NotaFiscal> notaFiscalLista = Arrays.asList(new NotaFiscal(), new NotaFiscal());
-        when(repository.findAllByNumero(NUMERO)).thenReturn(notaFiscalLista);
+        when(repository.findAllByNumero(NUMERO)).thenReturn(List.of(notaFiscal, notaFiscal));
 
-        List<NotaFiscalDto> notaFiscalDtos = service.buscarPorNumero(NUMERO);
-        assertNotNull(notaFiscalDtos);
-        assertEquals(2, notaFiscalDtos.size());
+        List<NotaFiscalDto> notasFiscais = service.buscarPorNumero(NUMERO);
+
+        assertNotNull(notasFiscais);
+        assertEquals(2, notasFiscais.size());
+        assertEquals(NotaFiscalDto.class, notasFiscais.get(0).getClass());
+        assertEquals(ID, notasFiscais.get(1).getId());        
         verify(repository, times(1)).findAllByNumero(NUMERO);
     }
 
     @Test
     public void listarTodos() {
-        List<NotaFiscal> notaFiscalLista = Arrays.asList(new NotaFiscal(), new NotaFiscal());
-        when(repository.findAll()).thenReturn(notaFiscalLista);
+        when(repository.findAll()).thenReturn(List.of(notaFiscal, notaFiscal));
 
-        List<NotaFiscalDto> notaFiscalDtos = service.listarTodos();
-        assertNotNull(notaFiscalDtos);
-        assertEquals(2, notaFiscalDtos.size());
+        List<NotaFiscalDto> notasFiscais = service.listarTodos();
+        
+        assertNotNull(notasFiscais);
+        assertEquals(2, notasFiscais.size());
+        assertEquals(NotaFiscalDto.class, notasFiscais.get(0).getClass());
+        assertEquals(ID, notasFiscais.get(1).getId());
         verify(repository, times(1)).findAll();
     }
 
     @Test
-    public void deveRemoverNotaFiscal() {
-        service.deletarPorId(1000);
-        verify(repository, times(1)).deleteById(1000);
+    public void  deveriaNaoDeletarNotaIdInexistente() {
+        when(repository.findById(anyInt())).thenReturn(Optional.empty());
+        
+        Exception excecao = assertThrows(NotaFiscalCabecalhoNaoEncontradaException.class, () -> {
+            service.deletarPorId(ID);
+        });
+            
+            assertEquals(NotaFiscalCabecalhoNaoEncontradaException.class, excecao.getClass());
+            assertEquals("Nota Fiscal não encontrada para o id " + ID, excecao.getMessage());
     }
+
+    @Test
+    public void deveRemoverNotaFiscal() {
+        when(repository.findById(anyInt())).thenReturn(notaFiscalOpcional);
+        doNothing().when(repository).deleteById(anyInt());
+        service.deletarPorId(ID);
+        verify(repository, times(1)).deleteById(anyInt());
+    }
+
+    private void inicializarNotaFiscal() {
+        notaFiscalTipo = new NotaFiscalTipo();
+        notaFiscalTipo.setNome(TIPO);
+
+        notaFiscal = new NotaFiscal();
+        notaFiscal.setId(ID);
+        notaFiscal.setNumero(NUMERO);
+        notaFiscal.setData(DATA);
+        notaFiscal.setValorTotal(VALORTOTAL);
+        notaFiscal.setItens(ITENS);
+        notaFiscal.setParceiroNegocio(fabricante);
+        notaFiscal.setNotaFiscalTipo(notaFiscalTipo);
+
+        notaFiscalDto = new NotaFiscalDto();
+        notaFiscalDto.setId(ID);
+        notaFiscalDto.setNumero(NUMERO);
+        notaFiscalDto.setData(DATA);
+        notaFiscalDto.setValorTotal(VALORTOTAL);
+        notaFiscalDto.setItens(ITENS);
+        notaFiscalDto.setParceiroNegocio(fabricante);
+        notaFiscalDto.setNotaFiscalTipo(notaFiscalTipo);
+
+        notaFiscalOpcional = Optional.of(notaFiscal);
+
+        when(mapper.converterParaEntidade(notaFiscalDto)).thenReturn(notaFiscal);
+        when(mapper.converterParaDto(notaFiscal)).thenReturn(notaFiscalDto);
+    }
+
 }
