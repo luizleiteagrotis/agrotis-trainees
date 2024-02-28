6278ad9778e7062b56f5ba45bae8a6a5cd6f40cd
“leticiavidottin” <leticiavidottin@gmail.com>
2024-02-28 17:14:54 -0300

    TP-198 wip(CrudTestesUnitarios): adicionada cobertura de testes unitários em 100% para NotaFiscalItemConversaoService.

--- crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalItemConversaoServiceTest.java
+++ crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalItemConversaoServiceTest.java
@@ -1,5 +1,88 @@
 package com.agrotis.trainees.crud.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+
+import com.agrotis.trainees.crud.dto.NotaFiscalDto;
+import com.agrotis.trainees.crud.dto.NotaFiscalItemDto;
+import com.agrotis.trainees.crud.dto.ProdutoDto;
+import com.agrotis.trainees.crud.entity.NotaFiscal;
+import com.agrotis.trainees.crud.entity.NotaFiscalItem;
+import com.agrotis.trainees.crud.entity.Produto;
+
+@RunWith(MockitoJUnitRunner.class)
 public class NotaFiscalItemConversaoServiceTest {
+
+    @Mock
+    private NotaFiscal nota;
+
+    @Mock
+    private NotaFiscalConversaoService conversaoNota;
+
+    @Mock
+    private ProdutoConversaoService conversaoProduto;
+
+    @Mock
+    private Produto produto;
+
+    @InjectMocks
+    private NotaFiscalItemConversaoService conversao;
+
+    @Before
+    public void setUp() {
+        // Configurar comportamento dos mocks, se necessário
+    }
+
+    @Test
+    @DisplayName("Teste para o método converterParaDto")
+    public void testeConverterDto() {
+        NotaFiscalItem entidade = new NotaFiscalItem();
+        entidade.setId(1);
+        entidade.setQuantidade(10);
+        entidade.setPrecoUnitario(BigDecimal.TEN);
+        entidade.setValorTotal(BigDecimal.valueOf(100));
+
+        when(conversaoNota.converterParaDto(nota)).thenReturn(new NotaFiscalDto());
+        when(conversaoProduto.converterParaDto(produto)).thenReturn(new ProdutoDto());
+
+        entidade.setNotaFiscal(nota);
+        entidade.setProduto(produto);
+
+        NotaFiscalItemDto dto = conversao.converterParaDto(entidade);
+
+        assertEquals(entidade.getId(), dto.getId());
+        assertEquals(entidade.getQuantidade(), dto.getQuantidade());
+        assertEquals(entidade.getPrecoUnitario(), dto.getPrecoUnitario());
+        assertEquals(entidade.getValorTotal(), dto.getValorTotal());
+    }
+
+    @Test
+    public void testConverterParaEntidade() {
+        NotaFiscalItemDto dto = new NotaFiscalItemDto();
+        dto.setId(1);
+        dto.setQuantidade(10);
+        dto.setPrecoUnitario(BigDecimal.TEN);
+        dto.setValorTotal(BigDecimal.valueOf(100));
+
+        when(conversaoNota.converterParaEntidade(dto.getNotaFiscal())).thenReturn(nota);
+        when(conversaoProduto.converterParaEntidade(dto.getProduto())).thenReturn(produto);
+
+        NotaFiscalItem entidade = conversao.converterParaEntidade(dto);
+
+        assertEquals(dto.getId(), entidade.getId());
+        assertEquals(dto.getQuantidade(), entidade.getQuantidade());
+        assertEquals(dto.getPrecoUnitario(), entidade.getPrecoUnitario());
+        assertEquals(dto.getValorTotal(), entidade.getValorTotal());
+    }
 
 }
--- crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalItemServiceTest.java
+++ crud/src/test/java/com/agrotis/trainees/crud/service/NotaFiscalItemServiceTest.java
@@ -1,5 +1,19 @@
 package com.agrotis.trainees.crud.service;
 
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.agrotis.trainees.crud.repository.NotaFiscalItemRepository;
+
+@RunWith(MockitoJUnitRunner.class)
 public class NotaFiscalItemServiceTest {
+
+    @Mock
+    private NotaFiscalItemRepository repository;
+
+    @InjectMocks
+    private NotaFiscalItemService service;
 
 }
